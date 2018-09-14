fe2c4fa17ddf0ec2c98d4e6dd72b7c395a34fe68
Daiya, Lokesh <lokesh.daiya@fisglobal.com>
2018-09-14 17:34:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Create Node CLI
+
+#Inspired by
+
+https://medium.com/northcoders/creating-a-project-generator-with-node-29e13b3cd309
+https://www.sitepoint.com/javascript-command-line-interface-cli-node-js/
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('This is the filesearch script.');
+#! /usr/bin/env node
+var  userArgs = process.argv.slice(2);
+var fileName = userArgs[0];
+var fs = require('fs');
+
+fs.writeFile('fileName.txt', fileName, (err) => {
+    if(err) throw err;
+    console.log('Create text file');
+});
